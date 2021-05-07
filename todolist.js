feba3ef727ecc87b424832f5cc0757dbd76255ac
zwawrzaszek <65920149+zwawrzaszek@users.noreply.github.com>
2021-05-06 21:29:39 -0400

    Added application files

---
+++ todolist.js
@@ -0,0 +1,45 @@
+//selecting dom elements for manipulation
+var input = document.querySelector("input[type = 'text']");
+var ul = document.querySelector("ul");
+var container = document.querySelector("div");
+var lists = document.querySelectorAll("li");
+var spans = document.getElementsByTagName("span");
+
+//function to delete todo if delete span is clicked.
+function deleteTodo(){
+  for(let span of spans){
+    span.addEventListener ("click",function (){
+      span.parentElement.remove();
+      event.stopPropagation();
+    });
+  }
+}
+
+
+//event listener for input to add new todo to the list.
+input.addEventListener("keypress",function(keyPressed){
+  if(keyPressed.which === 13){
+    //creating lists and span when enter is clicked
+    var li = document.createElement("li");
+    var spanElement = document.createElement("span");
+    var icon = document.createElement("i");
+        
+    var newTodo = this.value;
+    this.value = " " ;
+        
+    icon.classList.add('fas', 'fa-trash-alt');
+    spanElement.append(icon);
+    ul.appendChild(li).append(spanElement,newTodo);
+
+    deleteTodo();
+    
+    }
+    
+});
+
+
+//delete todo
+deleteTodo();
+
+//load Todo
+loadTodo();
